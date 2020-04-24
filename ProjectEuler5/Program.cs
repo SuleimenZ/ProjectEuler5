431f0dc1c73fa0860b0198df1c80ef9e52df7007
SuleimenZ <62009948+SuleimenZ@users.noreply.github.com>
2020-04-24 20:25:47 +0200

    Add project files.

---
+++ ProjectEuler5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            for (int i = 11*13*17*19;; i += 11*13*17*19)
+            {
+                if (i % 20 == 0 && i % 18 == 0 && i % 16 == 0 && i % 15 == 0 && i % 14 == 0&& i % 12 == 0)
+                {
+                    result = i;
+                    break;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
